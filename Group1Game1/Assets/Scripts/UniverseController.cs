815a3c77ad0f02eb4550c50fbfdd87a74c7d6903
Brigham Boice <brigham.boice@mymail.champlain.edu>
2025-09-21 21:47:57 -0400

    Adding in locking and universe positions lock

--- Group1Game1/Assets/Scripts/UniverseController.cs
+++ Group1Game1/Assets/Scripts/UniverseController.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class UniverseController : MonoBehaviour
@@ -10,25 +11,42 @@ public class UniverseController : MonoBehaviour
 
     [Header("Constraints")]
     [Min(0f), SerializeField] float minZoom = 1f;
+    public static float MinZoom;
     [Min(0f), SerializeField] float maxZoom = 5f;
+    public static float MaxZoom;
     [Min(0f), SerializeField] float maxX = 8f;
+    public static float MaxX;
     [Min(0f), SerializeField] float maxY = 5f;
+    public static float MaxY;
 
     [Header("References")]
     [SerializeField] Transform cameraTransform;
     [SerializeField] Camera cameraComponent;
+    [SerializeField] TMP_Text cameraText;
 
-    [SerializeField] Vector3 universePosition = Vector3.zero;
+    [SerializeField] Vector3 universePosition = Vector3.one;
     //Use this to see if camera is close to story point
     public Vector3 UniversePosition => universePosition;
 
     Vector2 moveInput;
     float zoomInput;
+
+    private void Awake()
+    {
+        MinZoom = minZoom;
+        MaxZoom = maxZoom;
+        MaxX = maxX;
+        MaxY = maxY;
+    }
+
     private void Update()
     {
         //Read input from keyboard or other input systems (clamped -1/1 for X, Y, and zoom)
         moveInput = ReadKeyInputMove();
         zoomInput = ReadZoomInputMove();
+
+        if (!canMove)
+            return;
 
         //Use this function here to apply movement and zoom to the camera
         ApplyMovement(moveInput, zoomInput);
@@ -36,15 +54,21 @@ public class UniverseController : MonoBehaviour
         //Update the universe position based on the camera position
         //Use this to see if camera is close to story point
         universePosition = new Vector3(cameraTransform.position.x, cameraTransform.position.y, cameraComponent.orthographicSize);
+        //cameraText.text = $"Day: {LocationInSpace.GetDayString(universePosition.x)} " +
+        //    $"Month: {LocationInSpace.GetMonthString(universePosition.y)} " +
+        //    $"Year: {universePosition.z:0.0}";
+        cameraText.text = $"Day: {universePosition.x:0.0} " +
+            $"Month: {universePosition.y:0.0} " +
+            $"Year: {universePosition.z:0.0}";
     }
 
     float currentZoom;
     float currentZoom01 => Mathf.InverseLerp(minZoom, maxZoom, currentZoom);
     float currentZoom10 => 1f - currentZoom01;
-        
+
     //This is where the zoom and movement is applied to the camera
     private void ApplyMovement(Vector2 input, float zoom)
-    { 
+    {
         //Zoom----
         float newZoom = cameraComponent.orthographicSize + zoom * Time.deltaTime;
         currentZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
@@ -59,7 +83,7 @@ public class UniverseController : MonoBehaviour
 
         //This is used to clamp the cmaera into the screen and keep in universe bounds
         float newX = Mathf.Clamp(cameraTransform.position.x + moveStepX, -maxX * currentZoom10, maxX * currentZoom10);
-        float newY = Mathf.Clamp(cameraTransform.position.y + moveStepY, -maxY * currentZoom10, maxY* currentZoom10);
+        float newY = Mathf.Clamp(cameraTransform.position.y + moveStepY, -maxY * currentZoom10, maxY * currentZoom10);
 
         float z = cameraTransform.position.z;
         cameraTransform.position = new Vector3(newX, newY, z);
@@ -92,5 +116,15 @@ public class UniverseController : MonoBehaviour
         }
 
         return zoom * zoomSpeed;
+    }
+
+    bool canMove = true;
+    internal void StopMovement()
+    {
+        canMove = false;
+    }
+    internal void StartMovement()
+    {
+        canMove = true;
     }
 }
